96ff70f6859a7eac6dd4a04ecb545d02e7afd21c
Ronen Mashal <ronen.mashal@gmail.com>
2022-07-16 19:45:33 +0300

    Refactorred 'test-predict' to load models from w&b.

--- model/test-predict.py
+++ model/test-predict.py
@@ -4,6 +4,25 @@ from PIL import Image as im
 import os
 import wandb_helpers as wbh
 
+class_names = ["T-shirt/top","Trouser","Pullover","Dress","Coat","Sandal","Shirt","Sneaker","Bag","Ankle boot"]
+
+def predict_image_label(model, image):
+    '''
+    Predict a label based on a given image.
+    The image must be an array of uint8, containing 28 * 28 = 784 items. 
+    The shape does not matter. It will be reshaped to the necessary form.
+    '''
+    image = image / 255.0
+    image = image.reshape([1, 28, 28])
+    predictions = model.predict(image)
+    label_idx = np.argmax(predictions[0])
+    prediction = {
+        "label id": label_idx,
+        "label": class_names[label_idx],
+        "certainty": predictions[0][label_idx]
+    }
+    return prediction
+
 img_idx = 23
 img_file_name = f"image{img_idx}.bin"
 if not os.path.isfile(img_file_name):
@@ -15,6 +34,7 @@ else:
     with open(img_file_name, "rb") as file:
         img = np.load(file)
 
-model = PredictionModel.create_prediction_model("173338")
-prediction = model.predict_image_label(img)
-print (prediction)
+with wbh.start_wandb_run("FCNN", None) as run:
+    model = wbh.read_model(run, "FCNN", "latest")
+    prediction = predict_image_label(model, img)
+    print (prediction)
